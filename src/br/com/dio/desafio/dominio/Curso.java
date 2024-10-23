af4e0c4dcb4cac5f8ac6a7a8b5022a784ce0646a
victorCNeves <concolato.neves@gmail.com>
2024-10-23 20:23:19 -0300

    criação inteface Conteudo, classes Curso e Mentoria

---
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -0,0 +1,28 @@
+package br.com.dio.desafio.dominio;
+
+public class Curso extends Conteudo{
+    private int cargaHoraria;
+
+    public Curso() {
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public double calcularXP() {
+        return XP_PADRAO*cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{'" + "titulo=" + getTitulo() + "', descricao=' " + getDescricao() +
+                "', carga horaria=" + cargaHoraria +
+                '}';
+    }
+}
